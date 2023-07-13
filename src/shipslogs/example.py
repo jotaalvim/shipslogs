9966a87651ae48d549676edde3374371b27abf4f
João Alvim <joaoafonsoalvim@gmail.com>
2023-07-13 14:10:38 +0100

    gui update

--- src/shipslogs/example.py
+++ src/shipslogs/example.py
@@ -82,13 +82,7 @@ class App(customtkinter.CTk):
         App.frames["frame2"].pack(in_=self.right_side_container,side=tkinter.TOP, fill=tkinter.BOTH, expand=True, padx=0, pady=0)
     
     def __init__(self):
-        global entry1,entry2,startB
-
-        global nameEntry
-        global pathIEntry
-        global pathOEntry
-        global dateFormatEntry
-        global save
+        global entry1, entry2, startB, nameEntry, pathIEntry, pathOEntry, dateFormatEntry, save
 
         super().__init__()
         
@@ -96,17 +90,6 @@ class App(customtkinter.CTk):
     
         self.geometry("{0}x{0}+0+0".format(self.winfo_screenwidth(), self.winfo_screenheight()))
     
-    
-        # left side panel -> for frame selection
-        left_side_panel = customtkinter.CTkFrame(self, width=150)
-        left_side_panel.pack(side=tkinter.LEFT, fill=tkinter.Y, expand=False, padx=10, pady=10)
-    
-        # buttons to select the frames
-        bt_frame1 = customtkinter.CTkButton(left_side_panel, text="Ship's Diary", command=self.frame1_selector,font=my_font)
-        bt_frame1.grid(row=0, column=0, padx=20, pady=10)
-        
-        bt_frame2 = customtkinter.CTkButton(left_side_panel, text="Settings", command=self.frame2_selector,font=my_font)
-        bt_frame2.grid(row=1, column=0, padx=20, pady=10)
 
 
         # right side panel -> to show the frame1 or frame 2
@@ -117,7 +100,6 @@ class App(customtkinter.CTk):
         self.right_side_container.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True, padx=0, pady=0)
     
 
-
         #===============================================================================
         # SHIPS DIARY
 
@@ -126,20 +108,29 @@ class App(customtkinter.CTk):
         #bt_from_frame1.place(relx=0.5, rely=0.5, anchor='n')
 
 
-        label = customtkinter.CTkLabel(App.frames['frame1'], text='Ship\'s Diary',font=('Roboto',26))
-        label.grid(row=0, column=0,pady=12,padx=12)
+        label = customtkinter.CTkLabel(App.frames['frame1'], text='Ship\'s Diary',font=('Roboto',40))
+        #label.grid(row=0, column=0,pady=12,padx=12)
+        label.place(relx=0.45, rely=0.1, anchor='w')
+
         
 
         entry1 = customtkinter.CTkEntry(App.frames['frame1'],placeholder_text='topic name',font=my_font)
-        entry1.grid(row=1, column=0,pady=12,padx=12)
+        #entry1.grid(row=1, column=0,pady=12,padx=12)
+        entry1.place(relx=0.45, rely=0.2, anchor='w')
+
 
         entry2 = customtkinter.CTkEntry(App.frames['frame1'],placeholder_text=loader.getDay(),font=my_font)
         entry2.grid(row=2, column=0,pady=12,padx=12)
+        entry2.place(relx=0.45, rely=0.3, anchor='w')
+
     
 
 
         startB = customtkinter.CTkButton(App.frames['frame1'], text='Start Diary',command=start,font=my_font)
-        startB.grid(row=3, column=0,pady=12,padx=12)
+        #startB.grid(row=3, column=0,pady=12,padx=12)
+        startB.place(relx=0.45, rely=0.3, anchor='w')
+
+
         if stop == False:
             startB.configure(fg_color='#FF7F7F',text='STOP Diary')
             top,sub = helpergui.getTopics()
@@ -153,54 +144,8 @@ class App(customtkinter.CTk):
         #settingsB = customtkinter.CTkButton(App.frames['frame1'], text='Settings',command=lambda:self.frame2_selector(),font=my_font,image= setting_image,compound ="left"  )
         # FIXME
         settingsB = customtkinter.CTkButton(App.frames['frame1'], text='Settings',command=lambda:self.frame2_selector(),font=my_font)
-        settingsB.grid(row=4, column=0,pady=12,padx=12)
-
-
-        def switch_event1():
-            helpergui.toogleFormat("pdf")
-
-        def switch_event2():
-            helpergui.toogleFormat("docx")
-
-        def switch_event3():
-            helpergui.toogleFormat("pptx")
-
-        def switch_event4():
-            helpergui.toogleFormat("html")
-
-        def switch_event5():
-            helpergui.toogleFormat("latex")
-
-
-
-
-        switch_1 = customtkinter.CTkSwitch(App.frames['frame1'], text="pdf", command=switch_event1,font=my_font)
-        switch_1.grid(row=0, column=1,padx=20, pady=10)
-
-        switch_2 = customtkinter.CTkSwitch(App.frames['frame1'], text="docx", command=switch_event2,font=my_font)
-        switch_2.grid(row=1, column=1,padx=20, pady=10)
-
-        switch_3 = customtkinter.CTkSwitch(App.frames['frame1'], text="pptx", command=switch_event3,font=my_font)
-        switch_3.grid(row=2, column=1,padx=20, pady=10)
-
-        switch_4 = customtkinter.CTkSwitch(App.frames['frame1'], text="html", command=switch_event4,font=my_font)
-        switch_4.grid(row=3, column=1,padx=20, pady=10)
-
-        switch_5 = customtkinter.CTkSwitch(App.frames['frame1'], text="latex", command=switch_event5,font=my_font)
-        switch_5.grid(row=4, column=1,padx=20, pady=10)
-
-
-        if helpergui.setFormat("pdf"):
-            switch_1.select(1)
-        if helpergui.setFormat("docx"):
-            switch_2.select(1)
-        if helpergui.setFormat("pptx"):
-            switch_3.select(1)
-        if helpergui.setFormat("html"):
-            switch_4.select(1)
-        if helpergui.setFormat("latex"):
-            switch_5.select(1)
-
+        #settingsB.grid(row=4, column=0,pady=12,padx=12)
+        settingsB.place(relx=0.45, rely=0.4, anchor='w')
 
 
 
@@ -211,14 +156,15 @@ class App(customtkinter.CTk):
         #bt_from_frame2 = customtkinter.CTkButton(App.frames['frame2'], text="Test 2", command=lambda:print("test 2") )
         #bt_from_frame2.place(relx=0.5, rely=0.5, anchor='w')
 
-
-
         aname, out, sin, date = helpergui.getSettings()
 
 
 
-        ships = customtkinter.CTkLabel(App.frames['frame2'], text='Ship\'s Diary Settings',font=('Roboto',26))
-        ships.grid(row=0, column=2,pady=12,padx=12)
+        ships = customtkinter.CTkLabel(App.frames['frame2'], text='Ship\'s Diary Settings',font=('Roboto',40))
+        #ships.grid(row=0, column=2,pady=12,padx=12)
+        ships.place(relx=0.5, rely=0.1, anchor='w')
+
+
 
         # FIXME label
         #labelS = customtkinter.CTkLabel(master=frame, text='Settings',font=my_font)
@@ -254,37 +200,59 @@ class App(customtkinter.CTk):
 
 
 
+        dateFormat = customtkinter.CTkLabel(App.frames['frame2'], text='Export formats',font=('Roboto',26))
+        dateFormat.grid(row=6, column=0,pady=12,padx=12)
+
+        def switch_event1():
+            helpergui.toogleFormat("pdf")
+
+        def switch_event2():
+            helpergui.toogleFormat("docx")
+
+        def switch_event3():
+            helpergui.toogleFormat("pptx")
+
+        def switch_event4():
+            helpergui.toogleFormat("html")
+
+        def switch_event5():
+            helpergui.toogleFormat("latex")
+
+
+        switch_1 = customtkinter.CTkSwitch(App.frames['frame2'], text="pdf", command=switch_event1,font=my_font)
+        switch_1.grid(row=7, column=1,padx=20, pady=10)
+
+        switch_2 = customtkinter.CTkSwitch(App.frames['frame2'], text="docx", command=switch_event2,font=my_font)
+        switch_2.grid(row=7, column=0,padx=20, pady=10)
+
+        switch_3 = customtkinter.CTkSwitch(App.frames['frame2'], text="pptx", command=switch_event3,font=my_font)
+        switch_3.grid(row=8, column=1,padx=20, pady=10)
+
+        switch_4 = customtkinter.CTkSwitch(App.frames['frame2'], text="html", command=switch_event4,font=my_font)
+        switch_4.grid(row=8, column=0,padx=20, pady=10)
+
+        switch_5 = customtkinter.CTkSwitch(App.frames['frame2'], text="latex", command=switch_event5,font=my_font)
+        switch_5.grid(row=9, column=0,padx=20, pady=10)
+
+
+
+        if helpergui.setFormat("pdf"):
+            switch_1.select(1)
+        if helpergui.setFormat("docx"):
+            switch_2.select(1)
+        if helpergui.setFormat("pptx"):
+            switch_3.select(1)
+        if helpergui.setFormat("html"):
+            switch_4.select(1)
+        if helpergui.setFormat("latex"):
+            switch_5.select(1)
+
         save = customtkinter.CTkButton(App.frames['frame2'], text='Save',command=saveSettings,font=my_font)
-        save.grid(row=6, column=3,pady=12,padx=12)
+        save.grid(row=10, column=3,pady=12,padx=12)
         #save.configure(fg_color='#7aeb7a',text_color = 'black')
 
         back = customtkinter.CTkButton(App.frames['frame2'], text='Back',command=lambda:self.frame1_selector(),font=my_font)
-        back.grid(row=6, column=0,pady=12,padx=12)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        back.grid(row=10, column=0,pady=12,padx=12)
 
 
 
